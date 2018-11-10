bc8a162cef65058b0d8ea8399d00b9137111187a
zhangyongming <zishixiuwen@126.com>
2018-11-10 16:42:51 +0800

    feat:隐藏无用模块

--- src/components/leftMenu/menu.config.js
+++ src/components/leftMenu/menu.config.js
@@ -5,36 +5,36 @@ const menuList = [
 		path: 'user',
 		authLevel: 1//权限等级： 1-普通管理员，2-超级管理员
 	},
-	{
-		index: 1,
-		name: '币生币项目管理',
-		path: 'coins',
-		authLevel: 1
-	},
-	{
-		index: 2,
-		name: '币生币购买记录',
-		path: 'coin_record',
-		authLevel: 1
-	},
-	{
-		index: 3,
-		name: '典当管理',
-		path: 'pawn',
-		authLevel: 1
-	},
-	{
-		index: 4,
-		name: '用户典当记录',
-		path: 'pawn_record',
-		authLevel: 1
-	},
-	{
-		index: 5,
-		name: '代持管理',
-		path: 'hold',
-		authLevel: 2
-	}
+	// {
+	// 	index: 1,
+	// 	name: '币生币项目管理',
+	// 	path: 'coins',
+	// 	authLevel: 1
+	// },
+	// {
+	// 	index: 2,
+	// 	name: '币生币购买记录',
+	// 	path: 'coin_record',
+	// 	authLevel: 1
+	// },
+	// {
+	// 	index: 3,
+	// 	name: '典当管理',
+	// 	path: 'pawn',
+	// 	authLevel: 1
+	// },
+	// {
+	// 	index: 4,
+	// 	name: '用户典当记录',
+	// 	path: 'pawn_record',
+	// 	authLevel: 1
+	// },
+	// {
+	// 	index: 5,
+	// 	name: '代持管理',
+	// 	path: 'hold',
+	// 	authLevel: 2
+	// }
 ];
 
 export default menuList;
--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,11 @@ import Router from 'vue-router';
 // import Home from '../pages/Home.vue'
 import Login from '../pages/login';
 import User from '../pages/user';
-import Coins from '../pages/coins';
-import Pawn from '../pages/pawn';
-import PawnRecord from '../pages/pawnRecords';
-import CoinRecord from '../pages/coinRecords';
-import Hold from '../pages/hold';
+// import Coins from '../pages/coins';
+// import Pawn from '../pages/pawn';
+// import PawnRecord from '../pages/pawnRecords';
+// import CoinRecord from '../pages/coinRecords';
+// import Hold from '../pages/hold';
 
 Vue.use(Router);
 
@@ -22,31 +22,31 @@ export default new Router({
 			path: '/user',
 			name: 'user',
 			component: User
-		},
-		{
-			path: '/pawn_record',
-			name: 'pawnRecord',
-			component: PawnRecord
-		},
-		{
-			path: '/coins',
-			name: 'Coins',
-			component: Coins
-		},
-		{
-			path: '/pawn',
-			name: 'Pawn',
-			component: Pawn
-		},
-		{
-			path: '/coin_record',
-			name: 'coinRecord',
-			component: CoinRecord
-		},
-		{
-			path: '/hold',
-			name: 'Hold',
-			component: Hold
 		}
+		// {
+		// 	path: '/pawn_record',
+		// 	name: 'pawnRecord',
+		// 	component: PawnRecord
+		// },
+		// {
+		// 	path: '/coins',
+		// 	name: 'Coins',
+		// 	component: Coins
+		// },
+		// {
+		// 	path: '/pawn',
+		// 	name: 'Pawn',
+		// 	component: Pawn
+		// },
+		// {
+		// 	path: '/coin_record',
+		// 	name: 'coinRecord',
+		// 	component: CoinRecord
+		// },
+		// {
+		// 	path: '/hold',
+		// 	name: 'Hold',
+		// 	component: Hold
+		// }
 	]
 });
