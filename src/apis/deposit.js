3998995c78df85bf918b47d5be981c1e5574a47c
Alex <875877496@qq.com>
2018-11-18 09:56:02 +0800

    Fix:默认资金流向页面不展示时不传时间字段

--- src/apis/deposit.js
+++ src/apis/deposit.js
@@ -4,35 +4,33 @@ import DOMAIN from './maps/DOMAIN';
 export default {
 	// 查询定存列表
 	getDepositList: params => {
-		let url = DOMAIN +
+		let url =
+			DOMAIN +
 			'qryDepositOverviewList?mock_apiId=5bead27741c2c429288308fc&pageNo=' +
 			params.pageNo +
 			'&pageSize=' +
 			params.pageSize;
 		if (params.startDate && params.endDate) {
-			url = url +
-				'&startDate=' +
-				params.startDate +
-				'&endDate=' +
-				params.endDate;
+			url +=
+				'&startDate=' + params.startDate + '&endDate=' + params.endDate;
 		}
 		return http.get(url);
 	},
 	// 查询资金流向列表
 	getPerformanceList: params => {
-		return http.get(
+		let url =
 			DOMAIN +
 			'qryPerformanceList?pageNo=' +
 			params.pageNo +
 			'&pageSize=' +
 			params.pageSize +
-			'&startDate=' +
-			params.startDate +
-			'&endDate=' +
-			params.endDate +
 			'&orderBy=' +
 			params.orderBy +
-			'&mock_apiId=5beacc78535e6d022778d6d0'
-		);
+			'&mock_apiId=5beacc78535e6d022778d6d0';
+		if (params.startDate && params.endDate) {
+			url +=
+				'&startDate=' + params.startDate + '&endDate=' + params.endDate;
+		}
+		return http.get(url);
 	}
 };
